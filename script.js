638f1af1ec11f7898e8c28581a889b761eeae128
n4msky <163948227+n4msky@users.noreply.github.com>
2024-08-12 21:40:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ function mostraPergunta() {
 }
 
 function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas) {
+        for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
